38cab41198435728c48a93e057c50042ce457123
GARAMY, Ben <U345267@WVA52157.int.Corp.sun>
2015-12-21 16:09:11 +1000

    Some new colour conversion functions

--- chart.py
+++ chart.py
@@ -1,7 +1,48 @@
 from PIL import Image
 import colorsys
+import colourx
 
 depth = 256
+
+colours = {}
+
+def hslum_gen():
+    for r in range(255):
+        for g in range(255):
+            for b in range(255):
+                print(str((r,g,b)))
+                hsv = colourx.rgb_to_hsv((r,g,b))
+                lum = colourx.get_lum((r,g,b))
+                colours[(hsv[0],hsv[1], lum)] = hsv
+
+class Colour():
+    def __init__(self, rgb):
+        self.rgb = rgb
+        #self.hsv = hsv
+        #self.lum = colourx.get_lum(rgb)
+        #self.hslum = (self.hsv[0], self.hsv[1], colourx.get_lum(rgb))
+
+def generate_color_map():
+    for r in range(255):
+        for g in range(255):
+            for b in range(255):
+                rgb = (r, g, b)
+                print("Generating "+str(rgb))
+                #hsv = colourx.rgb_to_hsv(rgb)
+                new_colour = Colour(rgb)
+                colours.append(new_colour)
+
+def print_colour_map():
+    chart = Image.new("RGB", (depth, depth))
+
+    x = y = 0
+    for colour in colours:
+        chart.putpixel((x,y), colour.rgb)
+        x+=1
+        if x==depth:
+            x = 0
+            y+=1
+    chart.save("assets/colormap.bmp")
 
 def print_color_chart():
     chart = Image.new("HSV", (depth, depth))
@@ -58,6 +99,63 @@ def print_hl_grey_chart():
 
     chart.convert("RGB").save("assets/hlgreychart.jpg")
 
+
+def print_hcl_chart():
+    chart = Image.new("RGB", (depth, depth))
+
+    for c in range(depth):
+        for h in range(depth):
+            rgb = colourx.hcl_to_rgb((h, c, 100))
+            chart.putpixel((h, depth-1-c), rgb)
+    chart.save("assets/hcl.bmp")
+
+def print_hcl_c_chart():
+    chart = Image.new("RGB", (depth, depth))
+
+    for l in range(depth):
+        for h in range(depth):
+            rgb = colourx.hcl_to_rgb((h, 100, l))
+            chart.putpixel((h, depth-1-l), rgb)
+    chart.save("assets/hclc.bmp")
+
+def print_hcl_cl_chart():
+    chart = Image.new("RGB", (depth, depth))
+
+    for c in range(depth):
+        for l in range(depth):
+            rgb = colourx.hcl_to_rgb((100, c, l))
+            chart.putpixel((c, depth-1-l), rgb)
+    chart.save("assets/hclcl.bmp")
+
+def print_hcl_hc_chart():
+    chart = Image.new("RGB", (depth, depth))
+
+    for c in range(depth):
+        for h in range(depth):
+            rgb = colourx.hcl_to_rgb((h, c, 200))
+            chart.putpixel((h, depth-1-c), rgb)
+    chart.save("assets/hclhl.bmp")
+
+def print_conversions():
+    chart = Image.new("HSV", (depth, depth))
+
+    for r in range(depth):
+        for g in range(depth):
+            hsv = colourx.rgb_to_hsv((r, g, 100))
+            chart.putpixel((r, depth-1-g), hsv)
+    chart.convert("RGB").save("assets/convert.bmp")
+
+def nice_rainbow():
+    chart = Image.new("HSV", (depth, 100))
+
+    for i in range(100):
+        for h in range(depth):
+            hsv = colourx.hcl_to_rgb((h, 200, 200))
+            chart.putpixel((h, 99-i), hsv)
+
+    chart.convert("RGB").save("assets/rainbow.bmp")
+
+
 class SCP():
     def __init__(self, color):
         self.rgb = color
@@ -71,5 +169,10 @@ if __name__ == "__main__":
     #print_color_chart()
     #print_grey_chart()
     #uniform_grey_chart()
-    print_hl_chart()
-    print_hl_grey_chart()
+    #print_hcl_cl_chart()
+    #print_hl_grey_chart()
+    #generate_color_map()
+    #print_colour_map()
+    #print_conversions()
+    #print_hcl_hc_chart()
+    nice_rainbow()
---
+++ colourx.py
@@ -0,0 +1,80 @@
+import math
+
+def rgb_to_hsv(rgb):
+    r = rgb[0]/255.0
+    g = rgb[1]/255.0
+    b = rgb[2]/255.0
+
+    M = max((r,g,b))
+    m = min((r,g,b))
+    chroma = M - m
+
+    h = 0
+    if chroma==0:
+        pass
+    elif M == r:
+        h = (g-b)/chroma % 6
+    elif M == g:
+        h = (b-r)/chroma + 2
+    else:
+        h = (r-g)/chroma + 4
+
+    v = M
+    s = 0 if chroma == 0 else chroma/v
+
+    #return (int(h*60), int(s*100), int(v*100))
+    return (int(h*42.5), int(s*255), int(v*255))
+
+def hcl_to_rgb(hcl):
+    h = hcl[0]
+    h = h/42.5
+    c = hcl[1]/255.0
+    l = hcl[2]/255.0
+
+    x = c*(1 - math.fabs(h%2-1))
+
+    rgb = (0,0,0)
+    if h < 1:
+        rgb = (c,x,0)
+    elif h < 2:
+        rgb = (x,c,0)
+    elif h < 3:
+        rgb = (0,c,x)
+    elif h < 4:
+        rgb = (0,x,c)
+    elif h < 5:
+        rgb = (x,0,c)
+    elif h <= 6:
+        rgb = (c,0,x)
+    m=l-(0.3*rgb[0] + 0.59*rgb[1] + 0.11*rgb[2])
+
+    rgb255 = []
+    for e in rgb:
+        rgb255.append(int((e+m)*255))
+    return (rgb255[0], rgb255[1], rgb255[2])
+
+def rgb_to_hcl(rgb):
+    r = rgb[0]/255.0
+    g = rgb[1]/255.0
+    b = rgb[2]/255.0
+
+    M = max((r,g,b))
+    m = min((r,g,b))
+    chroma = M - m
+
+    h = 0
+    if chroma==0:
+        pass
+    elif M == r:
+        h = ((g-b)/chroma%6)
+    elif M == g:
+        h = ((b-r)/2+2)
+    else:
+        h = (r-g)/chroma + 4
+
+    return (int(h*43.5), int(chroma*255), get_lum(rgb))
+
+
+def get_lum(rgb):
+    return int(math.sqrt(0.2126*rgb[0]**2 + 0.587*rgb[1]**2 + 0.114*rgb[2]**2))
+
